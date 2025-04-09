271b449bb61f35ea541f864ccb824015e824ad63
FrodoBaggins29 <102836187+FrodoBaggins29@users.noreply.github.com>
2025-04-09 15:28:35 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import github from '@astrojs/github';
+
 
 export default defineConfig({
   site: "https://FrodoBaggins29.github.io/Positivus",
