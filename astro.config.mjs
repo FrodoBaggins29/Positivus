95508bddfc5dc2e60c49598f7e5ec9c1af1444d3
FrodoBaggins29 <102836187+FrodoBaggins29@users.noreply.github.com>
2025-04-16 13:09:34 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import github from '@astrojs/github-pages';
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://frodobaggins29.github.io/Positivus",
-  integrations: [tailwind()],
-  output: 'static',
-  adapter: github(),
-});
+  site: 'https://frodoBaggins29.github.io',
+  base: 'Positivus',
+})
